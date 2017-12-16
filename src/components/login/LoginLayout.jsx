11da18ac758ddf4d8ef2bb2007d9a2417728ac55
xmatusk3 <445561@mail.muni.cz>
2017-12-16 08:45:41 +0100

    remove container-redux folder, connect Layout inside the component

--- src/components/login/LoginLayout.jsx
+++ src/components/login/LoginLayout.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as routes from '../../constants/routes';
 import LoginForm from './LoginForm';
-import { Loader } from '../../containers/shared/Loader.jsx';
+import Loader from '../../components/shared/Loader.jsx';
 import { FormBox } from './LoginForm.styles';
 // import { LoginHeader } from './LoginForm.styles';
 import { CenterDiv } from './LoginForm.styles';
@@ -16,9 +16,6 @@ const LoginLayout = ({ from }) => {
                         <CenterDiv>
                             <img style={{height:'256px', width:'256px'}} src={require('../../../static/assets/motivacneMyChat.jpg')} />
                         </CenterDiv>
-                        {/*<div style={{clear: 'both'}}>*/}
-                            {/*<LoginHeader>myChat</LoginHeader>*/}
-                        {/*</div>*/}
                         <div>
                             <LoginForm from={originalLocation} />
                         </div>
